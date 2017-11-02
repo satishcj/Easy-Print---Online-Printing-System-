9ca9adfe8b201f99b6237cf4576afcde2e54ed3f
alsgeek <32621481+alsgeek@users.noreply.github.com>
2017-11-02 23:44:07 +0530

    Update sql_import.php

--- sql/sql_import.php
+++ sql/sql_import.php
@@ -23,11 +23,14 @@ CREATE TABLE `accounts`.`users`
 (
     `id` INT NOT NULL AUTO_INCREMENT,
     `first_name` VARCHAR(50) NOT NULL,
-     `last_name` VARCHAR(50) NOT NULL,
+    `last_name` VARCHAR(50) NOT NULL,
     `email` VARCHAR(100) NOT NULL,
-    `password` VARCHAR(100) NOT NULL,
+	`password` VARCHAR(100) NOT NULL,
     `hash` VARCHAR(32) NOT NULL,
-    `active` BOOL NOT NULL DEFAULT 0,
+    `active` BOOLEAN NOT NULL DEFAULT 0,
+	`Contact` INT(50) NOT NULL,
+	`Address` VARCHAR(50) NOT NULL ,
+    
 PRIMARY KEY (`id`) 
 );') or die($mysqli->error);
 
